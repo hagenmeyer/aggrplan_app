58a83cf73c87b9dd6cb42ad5184431b93760a7fa
Hagen Meyer <hagen_meyer@gmx.de>
2012-01-10 14:00:28 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AggrplanApp::Application.config.secret_token = 'e908b2ea9c68f57461f10c830a225d4ed0bbfe52f941178d400e845570ec8e32c00fc70e08f99bdde06ed08b99cee0a20f816b8aff774d00103e3d40c5ff5163'
